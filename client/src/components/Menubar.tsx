76e5becf90723250ab1a6fef190452d83a85d6f0
mahiipal kachhad <mahipalkac@gmail.com>
2025-09-02 17:51:56 +0530

    adding menu

--- client/src/components/Menubar.tsx
+++ client/src/components/Menubar.tsx
@@ -1,7 +1,41 @@
+import { NavLink } from "react-router-dom";
+
 const Menubar = () => {
   return (
-    <div>
-      <h1>menubar </h1>
+    <div className="container mx-auto max-w-7xl flex justify-between items-center h-15 px-4">
+      <h1 className="text-2xl">E-Commerce</h1>
+      <ul className="flex justify-end align-middle gap-5">
+        <li>
+          <NavLink
+            to={"/login"}
+            className={({ isActive }) =>
+              `${isActive ? "bg-amber-500 text-white" : ""} px-5 py-1 rounded`
+            }
+          >
+            Login
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/register"}
+            className={({ isActive }) =>
+              `${isActive ? "bg-amber-500 text-white" : ""} px-5 py-1 rounded`
+            }
+          >
+            Register
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/dashboard"}
+            className={({ isActive }) =>
+              `${isActive ? "bg-amber-500 text-white" : ""} px-5 py-1 rounded`
+            }
+          >
+            Dashboard
+          </NavLink>
+        </li>
+      </ul>
     </div>
   );
 };
